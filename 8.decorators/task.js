d0b30e5f349c1d9f2b4edbfa978d309bc544815f
axer665 <vellsevul@mail.ru>
2023-04-27 15:01:36 +0300

    complete a task on the topic 'decorators'

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,47 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
+    const maxCacheValuesCount = 5;
+    return (...args) => {
+        let hash = {
+            value: args.join(","),
+            hashValue: md5(args.join(","))
+        }
   
+        if (cache.length > 5) {
+            cache.shift();
+        } 
+        
+        if (cache.find(item => item.value === hash.value)) {
+            return "Из кэша: " + func.apply(this, args);
+        }
+  
+        cache.push(hash);
+        return "Вычисляем: " + func.apply(this, args);
+    };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    
+    function wrapper(...args) {  
+      wrapper.allCount++;
   
+      if (wrapper.count === 0) {
+           func.apply(this, args);
+      }
+        
+      if (timeoutId === null){
+          wrapper.count++;
+          timeoutId = setTimeout(() => {
+              func.apply(this, args);
+          }, delay);
+      } else {
+          timeoutId = null;
+      }
+    }
+    return wrapper;
 }
